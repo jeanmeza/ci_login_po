fff479497f86dc434b123f3ae4d46cb681e670fc
Jean Meza <meza.jean@proton.me>
2026-06-01 22:28:10 +0200

    add CI for Maven build and configure headless Chrome for tests

--- src/test/java/tests/DriverLifeCycle.java
+++ src/test/java/tests/DriverLifeCycle.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class DriverLifeCycle {
 
@@ -11,8 +14,17 @@ public class DriverLifeCycle {
 
     @BeforeEach
     public void setUp() {
+        var options = new ChromeOptions();
+        options.addArguments(
+                "--remote-allow-origins=*",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--headless"
+        );
         // declaration and instantiation of objects/variables
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterEach
